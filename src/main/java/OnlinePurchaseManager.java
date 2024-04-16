a25d681b80edfde7f2218bcd59dd9c187080b148
kasen22 <143123847+kasen22@users.noreply.github.com>
2024-04-15 19:42:43 -0700

    Update OnlinePurchaseManager.java

--- src/main/java/OnlinePurchaseManager.java
+++ src/main/java/OnlinePurchaseManager.java
@@ -11,7 +11,13 @@ public class OnlinePurchaseManager
   */
   public int countElectronicsByMaker(String maker)
   {
-    /* to be implemented in part (a) */
+    int count = 0; 
+    for (int i = 0; i<purchases.size(); i++){
+      if(purchases.get(i).isElectronic() && purchases.get(i).getMaker().equals(maker)){
+        count = count + 1;
+      }
+    }
+    return count;
   }
   
   /** Returns true if any pair of adjacent purchased Gizmo objects are
