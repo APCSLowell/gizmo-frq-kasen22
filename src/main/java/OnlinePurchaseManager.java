0b239824335b0faf0eb768acea83445cb3873334
kasen22 <143123847+kasen22@users.noreply.github.com>
2024-04-15 19:44:22 -0700

    Update OnlinePurchaseManager.java

--- src/main/java/OnlinePurchaseManager.java
+++ src/main/java/OnlinePurchaseManager.java
@@ -25,7 +25,12 @@ public class OnlinePurchaseManager
   */
   public boolean hasAdjacentEqualPair()
   {
-    /* to be implemented in part (b) */
+    for(int i = 0; i < purchases.size()-1; i++){
+      if(purchases.get(i).equals(purchases.get(i+1))){
+        return true;
+      }
+    }
+    return false;
   }
   public OnlinePurchaseManager()
   {
